377c534ac698bbdca216bb9a58f7cdeb2088a6fa
schneutzi-81 <68896592+schneutzi-81@users.noreply.github.com>
2023-08-10 15:24:37 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberContent = document.getElementById("memberContent");
+    var skillsContent = document.getElementById("skillsContent");
+    var projectsContent = document.getElementById("projectsContent");
+    var contactContent = document.getElementById("contactContent");
+    var aboutContent = document.getElementById("aboutContent");
+    member.style.color = "#00A8E8";
+    skills.style.color = "#000000";
+    projects.style.color = "#000000";
+    contact.style.color = "#000000";
+    about.style.color = "#000000";
+    memberContent.style.display = "block";
+    skillsContent.style.display = "none";
+    projectsContent.style.display = "none";
+    contactContent.style.display = "none";
+    aboutContent.style.display = "none";
+}
